c6ceff2a97e8245ad06f056265e2220ee76e8366
Kerlyson Santos <kerlyson_sk@yahoo.com.br>
2019-10-29 13:59:49 -0300

    navegacao por nome

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -2,19 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:meals_app/category_meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
+  final String id;  
   final String title;
   final Color color;
 
-  const CategoryItem(this.title, this.color);
+  const CategoryItem(this.id, this.title, this.color);
 
   void _selecteCategory(BuildContext context) {
-    Navigator.of(context).push(
-      MaterialPageRoute(
-        builder: (_) {
-          return CategoryMealsScreen();
-        },
-      ),
+    Navigator.of(context).pushNamed(
+      '/category-meal',
+      arguments: {'id': id, 'title': title}
     );
+      // MaterialPageRoute(
+      //   builder: (_) {
+      //     return CategoryMealsScreen(id, title);
+      //   },
+      // ),
+    // );
   }
 
   @override
--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
-  const CategoryMealsScreen({Key key}) : super(key: key);
+  // final String categoryId;
+  // final String categoryTitle;
+
+  // const CategoryMealsScreen(this.categoryId, this.categoryTitle);
 
   @override
   Widget build(BuildContext context) {
+    final routeArgs =
+        ModalRoute.of(context).settings.arguments as Map<String, String>;
     return Scaffold(
-      appBar: AppBar(title: Text('Recipe'),),
-      body:Center(child: Text('The recipe for the category'),),
+      appBar: AppBar(
+          title: Text(
+        routeArgs['title'],
+      )),
+      body: Center(
+        child: Text('The recipe for the category'),
+      ),
     );
   }
 }
