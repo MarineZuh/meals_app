a38bef83e65e8d80c5e3e121373510b62a7467ff
Kerlyson Santos <kerlyson_sk@yahoo.com.br>
2019-11-02 15:23:22 -0300

    add drawer e tabs

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/screens/meal_detail_screen.dart';
+import 'package:meals_app/screens/tabs_screen.dart';
 
 import 'screens/categories_screen.dart';
 import 'screens/category_meals_screen.dart';
@@ -31,7 +32,7 @@ class MyApp extends StatelessWidget {
               ),
             ),
       ),
-      home: CategoriesScreen(),
+      home: TabsScreen (),
       routes: {
         '/category-meal': (ctx) => CategoryMealsScreen(),
         MealDetailScreen.routedName : (ctx) => MealDetailScreen(),
